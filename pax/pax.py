6a0b5bae8a9f5a7fd6bb0a3b79afd0fefd046653
Brandon Wallace <Brandon@Brandon-SP2.(none)>
2014-05-07 12:10:25 -0500

    fully interprets input

--- pax/pax.py
+++ pax/pax.py
@@ -1,29 +1,117 @@
 import sys
 proposers=[]
 acceptors=[]
-
-class Props: #This is a class for proposers
-    def __init__(self,num):
+mtypes=['PROP','PREP','PROM','ACC','ACCD','REJ']
+ctypes=['P','A']
+class C: 
+    def __init__(self,num,ty):
         self.num=num
+        assert ctypes.count(ty)>0
+        self.ty=ty
         self.failed=False
         #self.state=None    ##For future functionality
     def fail(self):
-        if self.failed==True:
-            print "Error, proposer %d was already failed" % self.num
         self.failed=True
     def recover(self):
-        if self.failed==False:
-            print "Error, proposer %d was already running" % self.num
         self.failed=False
-        
+    def display(self):
+        print " -----------------"
+        print "|   %s # %d  |" % (("Proposer" if self.ty == 'P' else "Acceptor"), self.num)
+        print "|   Failed?  %s   |" % ("yes" if self.failed==True else "no")
+        print " -----------------"
+
+class Message:
+    def __init__(self,ty,dest,source,propid,prior=None,val=None):
+        assert mtypes.index(ty)>=0
+        self.kind=ty
+        self.dst=dest
+        self.src=source
+        self.propid=propid
+        self.prior=prior
+        self.val=val
+    def display(self):
+        print "/////////////////////////////"
+        print "Message of type %s" % self.kind
+        print "from"
+        C.display(self.src)
+        print "to"
+        C.display(self.dst)
+        print "Proposal ID: %d" % self.propid
+        print "Prior Proposal?", self.prior
+        print "Value?", self.val
+        print "/////////////////////////////"
+    def minidisplay(self):
+        print "%d: Msg type %s from %s%d to %s%d" % (self.propid,
+                                                     self.kind,
+                                                     self.src.ty,
+                                                     self.src.num,
+                                                     self.dst.ty,
+                                                     self.dst.num)
+class Network:
+    def __init__(self):
+        self.counter=0
+        self.queue=[]
+    def enq(self,msg):
+        self.queue.append(msg)
+    def deq(self):
+        for i in self.queue:
+            if i.src.failed==True or i.dst.failed==True:
+                return None
+            self.queue.pop(0)
+    def display(self):
+        print "-----Network:-----"
+        for i in self.queue:
+            Message.minidisplay(i)
+        print "------------------"
+class Event:
+    def __init__(self,string):
+        x=string.split()
+        self.tick=int(x[0])
+        assert x[1]=="PROPOSE" or x[1]=="FAIL" or x[1]=="RECOVER"
+        self.ty=x[1][0] # ie, ty will be 'P' or 'F' or 'R'
+        if self.ty=="P":
+            self.value=int(x[3])
+            self.c = proposers[int(x[2])-1]
+        else:
+            self.c = proposers[int(x[3])-1] if x[2]=="PROPOSER" else acceptors[int(x[3])-1]
+            self.value=None
+
+    
+    def display(self):
+        print "%d: Event type %s" % (self.tick, self.ty)
+        if self.ty == 'P':
+            print "(Proposer %d proposes %d)\n" % (self.c.num, self.value)
+        else:
+            print "(%s %s %d)\n" % ( ("Fail" if self.ty=='F' else "Recover"),
+                                   self.c.ty, self.c.num)
+
+def whois():
+    for p in proposers:
+        C.display(p)
+    for a in acceptors:
+        C.display(a)
 def initialize(): #will make a list of valid input file
     L=[]
     for line in sys.stdin:
-        print line
-        if line[0]!='#' and len(line.split())>1:
-            if line.split()[1]=="END":
-                return L
+        #print line
+        if line[0]!='#' and len(line.split())>1 and line.split()[1]!="END":
             L.append(line.rstrip())
     
+    setupvars=L[0].split()
+    for i in range(0,int(setupvars[0])):
+        proposers.append(C(i+1,'P'))
+    for i in range(0,int(setupvars[1])):
+        acceptors.append(C(i+1,'A'))
+    return L
 
-print initialize()
+
+
+
+def main():
+    L=initialize()
+    n=Network()
+    E=[]
+    for i in range(1,len(L)):
+        E.append(Event(L[i]))
+
+main()
